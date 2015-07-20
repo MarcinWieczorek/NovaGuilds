996fec499f082449d802261732b04c517dae6847
Marcin <marcin@marcin.co>
2015-07-20 04:02:03 +0200

    Removed exception handler

--- src/main/java/co/marcin/novaguilds/util/ExceptionHandler.java
+++
@@ -1,9 +0,0 @@
-package co.marcin.novaguilds.util;
-
-public class ExceptionHandler implements Thread.UncaughtExceptionHandler {
-
-	@Override
-	public void uncaughtException(Thread thread, Throwable throwable) {
-		LoggerUtils.exception(throwable);
-	}
-}
