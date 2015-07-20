bff4d026f0db75221962ffb93ed8b64924912e9c
Marcin <marcin@marcin.co>
2015-07-20 23:22:02 +0200

    PlayerInfo done

--- src/main/java/co/marcin/novaguilds/listener/PlayerInfoListener.java
+++ src/main/java/co/marcin/novaguilds/listener/PlayerInfoListener.java
@@ -1,10 +1,15 @@
 package co.marcin.novaguilds.listener;
 
 import co.marcin.novaguilds.NovaGuilds;
+import co.marcin.novaguilds.basic.NovaPlayer;
+import co.marcin.novaguilds.enums.Message;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
+
+import java.util.HashMap;
 
 public class PlayerInfoListener implements Listener {
 	private final NovaGuilds plugin;
@@ -14,12 +19,28 @@ public class PlayerInfoListener implements Listener {
 		plugin.getServer().getPluginManager().registerEvents(this,plugin);
 	}
 
-	@EventHandler
+	@EventHandler(priority = EventPriority.HIGH)
 	public void onPlayerClickPlayer(PlayerInteractEntityEvent event) {
+		Player player = event.getPlayer();
 		if(event.getRightClicked() instanceof Player) {
 			if(event.getPlayer().hasPermission("novaguilds.playerinfo")) { //TODO better permission node
 				Player clickedPlayer = (Player) event.getRightClicked();
-				//TODO code this!
+				NovaPlayer nCPlayer = plugin.getPlayerManager().getPlayer(clickedPlayer);
+
+				HashMap<String, String> vars = new HashMap<>();
+				vars.put("PLAYERNAME", nCPlayer.getName());
+				vars.put("POINTS", String.valueOf(nCPlayer.getPoints()));
+				vars.put("KILLS", String.valueOf(nCPlayer.getKills()));
+				vars.put("DEATHS", String.valueOf(nCPlayer.getDeaths()));
+				vars.put("KDR", String.valueOf(nCPlayer.getKills() / (nCPlayer.getDeaths() == 0 ? 1 : nCPlayer.getDeaths())));
+
+				if(nCPlayer.hasGuild()) {
+					vars.put("GUILDNAME", nCPlayer.getGuild().getName());
+					vars.put("TAG", plugin.tagUtils.getTag(nCPlayer.getPlayer()));
+				}
+
+				Message.CHAT_PLAYER_INFO_HEADER.send(player);
+				Message.CHAT_PLAYER_INFO_ITEMS.list().vars(vars).send(player);
 			}
 		}
 	}
