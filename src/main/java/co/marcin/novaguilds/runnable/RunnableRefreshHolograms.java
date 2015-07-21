23ea4bd5393db9e5bab1d5c42ba6fcc8347d54ee
Marcin <marcin@marcin.co>
2015-07-21 23:24:25 +0200

    Runnable for future use

---
+++ src/main/java/co/marcin/novaguilds/runnable/RunnableRefreshHolograms.java
@@ -0,0 +1,16 @@
+package co.marcin.novaguilds.runnable;
+
+import co.marcin.novaguilds.NovaGuilds;
+
+public class RunnableRefreshHolograms implements Runnable {
+	private final NovaGuilds plugin;
+
+	public RunnableRefreshHolograms(NovaGuilds novaGuilds) {
+		plugin = novaGuilds;
+	}
+
+	@Override
+	public void run() {
+		//TODO
+	}
+}
