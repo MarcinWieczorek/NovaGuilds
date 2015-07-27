c915c2246618ab70a5d8a331e20d130e005a088f
Marcin <marcin@marcin.co>
2015-07-28 00:05:19 +0200

    Fix: wrong permission for rg tp

--- src/main/java/co/marcin/novaguilds/command/CommandAdminRegionTeleport.java
+++ src/main/java/co/marcin/novaguilds/command/CommandAdminRegionTeleport.java
@@ -21,7 +21,7 @@ public class CommandAdminRegionTeleport implements CommandExecutor {
 	}
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if(!sender.hasPermission("novaguilds.admin.region.delete")) {
+		if(!sender.hasPermission("novaguilds.admin.region.tp")) {
 			Message.CHAT_NOPERMISSIONS.send(sender);
 			return true;
 		}
