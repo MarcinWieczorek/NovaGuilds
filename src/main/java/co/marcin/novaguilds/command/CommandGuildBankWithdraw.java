c384e089b714d8239bba27e463e869ac8701e7e5
Marcin <marcin@marcin.co>
2015-08-04 03:46:47 +0200

    CommandGuildBankWithdraw using new executor

--- src/main/java/co/marcin/novaguilds/command/CommandGuildBankWithdraw.java
+++ src/main/java/co/marcin/novaguilds/command/CommandGuildBankWithdraw.java
@@ -1,38 +1,39 @@
 package co.marcin.novaguilds.command;
 
-import co.marcin.novaguilds.NovaGuilds;
 import co.marcin.novaguilds.basic.NovaGuild;
 import co.marcin.novaguilds.basic.NovaPlayer;
+import co.marcin.novaguilds.enums.Commands;
 import co.marcin.novaguilds.enums.Message;
+import co.marcin.novaguilds.interfaces.Executor;
 import co.marcin.novaguilds.util.NumberUtils;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import java.util.HashMap;
 
-public class CommandGuildBankWithdraw implements CommandExecutor {
-	private final NovaGuilds plugin;
-	
-	public CommandGuildBankWithdraw(NovaGuilds pl) {
-		plugin = pl;
+public class CommandGuildBankWithdraw implements Executor {
+	private final Commands command;
+
+	public CommandGuildBankWithdraw(Commands command) {
+		this.command = command;
+		plugin.getCommandManager().registerExecutor(command, this);
 	}
-	
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if(!sender.hasPermission("novaguilds.guild.bank.withdraw")) {
-			Message.CHAT_NOPERMISSIONS.send(sender);
-			return true;
+
+	@Override
+	public void execute(CommandSender sender, String[] args) {
+		if(!command.allowedSender(sender)) {
+			Message.CHAT_CMDFROMCONSOLE.send(sender);
+			return;
 		}
 
 		if(!(sender instanceof Player)) {
 			Message.CHAT_CMDFROMCONSOLE.send(sender);
-			return true;
+			return;
 		}
 
 		if(args.length != 1) {
 			Message.CHAT_GUILD_BANK_ENTERAMOUNT.send(sender);
-			return true;
+			return;
 		}
 
 		String moneyString = args[0];
@@ -40,19 +41,19 @@ public class CommandGuildBankWithdraw implements CommandExecutor {
 
 		if(!nPlayer.hasGuild()) {
 			Message.CHAT_GUILD_NOTINGUILD.send(sender);
-			return true;
+			return;
 		}
 
 		NovaGuild guild = nPlayer.getGuild();
 
 		if(!nPlayer.isLeader()) {
 			Message.CHAT_GUILD_BANK_WITHDRAW_NOTLEADER.send(sender);
-			return true;
+			return;
 		}
 
 		if(!NumberUtils.isNumeric(moneyString)) {
 			Message.CHAT_ENTERINTEGER.send(sender);
-			return true;
+			return;
 		}
 
 		double money = Double.parseDouble(moneyString);
@@ -60,12 +61,12 @@ public class CommandGuildBankWithdraw implements CommandExecutor {
 
 		if(money < 0) {
 			Message.CHAT_BASIC_NEGATIVENUMBER.send(sender);
-			return true;
+			return;
 		}
 
 		if(guild.getMoney() < money) {
 			Message.CHAT_GUILD_BANK_WITHDRAW_NOTENOUGH.send(sender);
-			return true;
+			return;
 		}
 
 		guild.takeMoney(money);
@@ -74,7 +75,6 @@ public class CommandGuildBankWithdraw implements CommandExecutor {
 		HashMap<String,String> vars = new HashMap<>();
 		vars.put("AMOUNT",money+"");
 		Message.CHAT_GUILD_BANK_WITHDRAW_SUCCESS.vars(vars).send(sender);
-		return true;
 	}
 
 }
