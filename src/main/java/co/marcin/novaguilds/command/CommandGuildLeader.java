61817752f6a9dfc9c6254d15b523e18a40b96f33
Marcin <marcin@marcin.co>
2015-08-04 03:47:23 +0200

    CommandGuildLeader using new executor

--- src/main/java/co/marcin/novaguilds/command/CommandGuildLeader.java
+++ src/main/java/co/marcin/novaguilds/command/CommandGuildLeader.java
@@ -2,26 +2,37 @@ package co.marcin.novaguilds.command;
 
 import java.util.HashMap;
 
+import co.marcin.novaguilds.enums.Commands;
 import co.marcin.novaguilds.enums.Message;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
+import co.marcin.novaguilds.interfaces.Executor;
 import org.bukkit.command.CommandSender;
 
 import co.marcin.novaguilds.basic.NovaGuild;
-import co.marcin.novaguilds.NovaGuilds;
 import co.marcin.novaguilds.basic.NovaPlayer;
 
-public class CommandGuildLeader implements CommandExecutor {
-	private final NovaGuilds plugin;
-	 
-	public CommandGuildLeader(NovaGuilds plugin) {
-		this.plugin = plugin;
+public class CommandGuildLeader implements Executor {
+	private final Commands command;
+
+	public CommandGuildLeader(Commands command) {
+		this.command = command;
+		plugin.getCommandManager().registerExecutor(command, this);
 	}
-	
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+	@Override
+	public void execute(CommandSender sender, String[] args) {
+		if(!command.hasPermission(sender)) {
+			Message.CHAT_NOPERMISSIONS.send(sender);
+			return;
+		}
+
+		if(!command.allowedSender(sender)) {
+			Message.CHAT_CMDFROMCONSOLE.send(sender);
+			return;
+		}
+
 		if(args.length != 1) {
 			Message.CHAT_PLAYER_ENTERNAME.send(sender);
-			return true;
+			return;
 		}
 
 		NovaPlayer nPlayer = plugin.getPlayerManager().getPlayer(sender);
@@ -29,29 +40,29 @@ public class CommandGuildLeader implements CommandExecutor {
 
 		if(newLeader == null) {
 			Message.CHAT_PLAYER_NOTEXISTS.send(sender);
-			return true;
+			return;
 		}
 
 		if(!nPlayer.hasGuild()) {
 			Message.CHAT_GUILD_NOTINGUILD.send(sender);
-			return true;
+			return;
 		}
 
 		NovaGuild guild = nPlayer.getGuild();
 
 		if(!nPlayer.isLeader()) {
 			Message.CHAT_GUILD_NOTLEADER.send(sender);
-			return true;
+			return;
 		}
 
 		if(newLeader.equals(nPlayer)) {
 			Message.CHAT_GUILD_LEADER_SAMENICK.send(sender);
-			return true;
+			return;
 		}
 
 		if(!newLeader.hasGuild() || !guild.isMember(newLeader)) {
 			Message.CHAT_GUILD_LEADER_NOTSAMEGUILD.send(sender);
-			return true;
+			return;
 		}
 
 		//set guild leader
@@ -66,7 +77,5 @@ public class CommandGuildLeader implements CommandExecutor {
 
 		//Tab and tags
 		plugin.tagUtils.refreshAll();
-		
-		return true;
 	}
 }
