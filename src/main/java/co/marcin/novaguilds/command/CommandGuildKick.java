72a0024b15c3b02b1b009987613c1ae9b613831a
Marcin <marcin@marcin.co>
2015-08-04 03:47:16 +0200

    CommandGuildKick using new executor

--- src/main/java/co/marcin/novaguilds/command/CommandGuildKick.java
+++ src/main/java/co/marcin/novaguilds/command/CommandGuildKick.java
@@ -1,67 +1,73 @@
 package co.marcin.novaguilds.command;
 
-import co.marcin.novaguilds.NovaGuilds;
 import co.marcin.novaguilds.basic.NovaGuild;
 import co.marcin.novaguilds.basic.NovaPlayer;
+import co.marcin.novaguilds.enums.Commands;
 import co.marcin.novaguilds.enums.Message;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
+import co.marcin.novaguilds.interfaces.Executor;
 import org.bukkit.command.CommandSender;
 
 import java.util.HashMap;
 
-public class CommandGuildKick  implements CommandExecutor {
-	private final NovaGuilds plugin;
-	
-	public CommandGuildKick(NovaGuilds novaGuilds) {
-		plugin = novaGuilds;
+public class CommandGuildKick implements Executor {
+	private final Commands command;
+
+	public CommandGuildKick(Commands command) {
+		this.command = command;
+		plugin.getCommandManager().registerExecutor(command, this);
 	}
-	
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if(!sender.hasPermission("novaguilds.guild.kick")) {
+
+	@Override
+	public void execute(CommandSender sender, String[] args) {
+		if(!command.hasPermission(sender)) {
 			Message.CHAT_NOPERMISSIONS.send(sender);
-			return true;
+			return;
 		}
-		
+
+		if(!command.allowedSender(sender)) {
+			Message.CHAT_CMDFROMCONSOLE.send(sender);
+			return;
+		}
+				
 		NovaPlayer nPlayer = plugin.getPlayerManager().getPlayer(sender);
 		
 		if(!nPlayer.hasGuild()) {
 			Message.CHAT_GUILD_NOTINGUILD.send(sender);
-			return true;
+			return;
 		}
 		
 		NovaGuild guild = nPlayer.getGuild();
 		
 		if(!guild.getLeader().getName().equalsIgnoreCase(sender.getName())) {
 			Message.CHAT_GUILD_NOTLEADER.send(sender);
-			return true;
+			return;
 		}
 		
 		if(args.length == 0) {
 			Message.CHAT_PLAYER_ENTERNAME.send(sender);
-			return true;
+			return;
 		}
 		
 		NovaPlayer nPlayerKick = plugin.getPlayerManager().getPlayer(args[0]);
 		
 		if(nPlayerKick == null) {
 			Message.CHAT_PLAYER_NOTEXISTS.send(sender);
-			return true;
+			return;
 		}
 
 		if(!nPlayerKick.hasGuild()) {
 			Message.CHAT_PLAYER_HASNOGUILD.send(sender);
-			return true;
+			return;
 		}
 		
 		if(!nPlayerKick.getGuild().getName().equalsIgnoreCase(guild.getName())) {
 			Message.CHAT_PLAYER_NOTINYOURGUILD.send(sender);
-			return true;
+			return;
 		}
 
 		if(nPlayer.getName().equalsIgnoreCase(nPlayerKick.getName())) {
 			Message.CHAT_GUILD_KICKYOURSELF.send(sender);
-			return true;
+			return;
 		}
 		
 		//all passed
@@ -80,7 +86,5 @@ public class CommandGuildKick  implements CommandExecutor {
 		
 		//tab/tag
 		plugin.tagUtils.refreshAll();
-		
-		return true;
 	}
 }
