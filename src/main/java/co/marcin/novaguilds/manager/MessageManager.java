da71875bcd597bfe004f085b3603ae1e6ec9a799
Marcin <marcin@marcin.co>
2015-08-14 01:45:11 +0200

    MessageManager methods are static now

--- src/main/java/co/marcin/novaguilds/manager/MessageManager.java
+++ src/main/java/co/marcin/novaguilds/manager/MessageManager.java
@@ -6,6 +6,7 @@ import co.marcin.novaguilds.enums.Message;
 import co.marcin.novaguilds.util.LoggerUtils;
 import co.marcin.novaguilds.util.StringUtils;
 import co.marcin.novaguilds.util.Title;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -20,17 +21,18 @@ import java.util.List;
 public class MessageManager {
 	private final NovaGuilds plugin;
 	private FileConfiguration messages = null;
-	private String prefix;
-	private ChatColor prefixColor = ChatColor.WHITE;
-	private String lang;
+	public String prefix;
+	public ChatColor prefixColor = ChatColor.WHITE;
+	public static MessageManager instance;
 
 	public MessageManager(NovaGuilds novaGuilds) {
 		plugin = novaGuilds;
+		instance = this;
 	}
 
 	public boolean loadMessages() {
 		setupDirectories();
-		lang = plugin.getConfig().getString("lang");
+		String lang = plugin.getConfig().getString("lang");
 		File messagesFile = new File(plugin.getDataFolder() + "/lang", lang + ".yml");
 		if(!messagesFile.exists()) {
 			if(plugin.getResource("lang/" + lang + ".yml") != null) {
@@ -70,8 +72,8 @@ public class MessageManager {
 	}
 
 	//set string from file
-	public String getMessagesString(String path) {
-		String msg = StringUtils.fixColors(getMessages().getString(path));
+	public static String getMessagesString(String path) {
+		String msg = StringUtils.fixColors(instance.getMessages().getString(path));
 
 		if(msg == null) {
 			return path;
@@ -86,20 +88,20 @@ public class MessageManager {
 	}
 
 	//send string with prefix to a player
-	public void sendPrefixMessage(Player p, String msg) {
-		p.sendMessage(StringUtils.fixColors(prefix + msg));
+	public static void sendPrefixMessage(Player p, String msg) {
+		p.sendMessage(StringUtils.fixColors(instance.prefix + msg));
 	}
 
-	public void sendPrefixMessage(CommandSender sender, String msg) {
-		sender.sendMessage(StringUtils.fixColors(prefix + msg));
+	public static void sendPrefixMessage(CommandSender sender, String msg) {
+		sender.sendMessage(StringUtils.fixColors(instance.prefix + msg));
 	}
 
-	public void sendMessage(CommandSender sender, String msg) {
+	public static void sendMessage(CommandSender sender, String msg) {
 		sender.sendMessage(StringUtils.fixColors(msg));
 	}
 
-	public void sendMessagesList(CommandSender sender, String path, HashMap<String,String> vars, boolean prefix) {
-		List<String> list = messages.getStringList(path);
+	public static void sendMessagesList(CommandSender sender, String path, HashMap<String,String> vars, boolean prefix) {
+		List<String> list = instance.messages.getStringList(path);
 
 		if(list != null) {
 			for(String msg : list) {
@@ -117,17 +119,17 @@ public class MessageManager {
 		}
 	}
 
-	public void sendMessagesMsg(CommandSender sender, String path) {
+	public static void sendMessagesMsg(CommandSender sender, String path) {
 		sendMessagesMsg(sender, path, false);
 	}
 
-	public void sendMessagesMsg(CommandSender sender, Message message, HashMap<String,String> vars) {
+	public static void sendMessagesMsg(CommandSender sender, Message message, HashMap<String,String> vars) {
 		sendMessagesMsg(sender, message.getPath(), vars, message.getTitle());
 	}
 
-	public void sendMessagesMsg(CommandSender sender, String path, boolean title) {
+	public static void sendMessagesMsg(CommandSender sender, String path, boolean title) {
 		String msg = getMessagesString(path);
-		if(plugin.getConfigManager().useTitles() && title && sender instanceof Player) {
+		if(NovaGuilds.getInstance().getConfigManager().useTitles() && title && sender instanceof Player) {
 			sendTitle((Player) sender, msg);
 		}
 		else {
@@ -135,15 +137,15 @@ public class MessageManager {
 		}
 	}
 
-	public void sendMessagesMsg(CommandSender sender, String path, HashMap<String,String> vars) {
+	public static void sendMessagesMsg(CommandSender sender, String path, HashMap<String,String> vars) {
 		sendMessagesMsg(sender,path,vars,true);
 	}
 
-	public void sendMessagesMsg(CommandSender sender, String path, HashMap<String,String> vars, boolean title) {
+	public static void sendMessagesMsg(CommandSender sender, String path, HashMap<String, String> vars, boolean title) {
 		String msg = getMessagesString(path);
 		msg = StringUtils.replaceMap(msg, vars);
 		//sendPrefixMessage(sender,msg);
-		if(plugin.getConfigManager().useTitles() && title && sender instanceof Player) {
+		if(NovaGuilds.getInstance().getConfigManager().useTitles() && title && sender instanceof Player) {
 			sendTitle((Player) sender, msg);
 		}
 		else {
@@ -151,43 +153,43 @@ public class MessageManager {
 		}
 	}
 
-	public void sendTitle(Player player, String msg) {
+	public static void sendTitle(Player player, String msg) {
 		Title title = new Title("");
-		title.setSubtitleColor(prefixColor);
+		title.setSubtitleColor(instance.prefixColor);
 		title.setSubtitle(StringUtils.fixColors(msg));
 		title.send(player);
 	}
 
 	//broadcast string to all players
-	public void broadcast(String msg) {
-		for(Player p : plugin.getServer().getOnlinePlayers()) {
+	public static void broadcast(String msg) {
+		for(Player p : Bukkit.getServer().getOnlinePlayers()) {
 			sendPrefixMessage(p, msg);
 		}
 	}
 
 	//broadcast message from file to all players with permission
-	public void broadcastMessageForPermitted(String path, String permission) {
-		for(Player p : plugin.getServer().getOnlinePlayers()) {
+	public static void broadcastMessageForPermitted(String path, String permission) {
+		for(Player p : Bukkit.getServer().getOnlinePlayers()) {
 			if(p.hasPermission(permission)) {
 				sendMessagesMsg(p,path);
 			}
 		}
 	}
 
-	public void broadcastMessage(Message message, HashMap<String,String> vars) {
+	public static void broadcastMessage(Message message, HashMap<String,String> vars) {
 		broadcastMessage(message.getPath(), vars);
 	}
 
-	public void broadcastMessage(String path,HashMap<String,String> vars) {
+	public static void broadcastMessage(String path, HashMap<String, String> vars) {
 		String msg = getMessagesString(path);
 		msg = StringUtils.replaceMap(msg, vars);
 
-		for(Player p : plugin.getServer().getOnlinePlayers()) {
+		for(Player p : Bukkit.getServer().getOnlinePlayers()) {
 			sendPrefixMessage(p, msg);
 		}
 	}
 
-	public void broadcastGuild(NovaGuild guild, Message message, HashMap<String,String> vars, boolean prefix) {
+	public static void broadcastGuild(NovaGuild guild, Message message, HashMap<String,String> vars, boolean prefix) {
 		String msg = getMessagesString(message.getPath());
 		msg = StringUtils.replaceMap(msg, vars);
 
@@ -201,25 +203,7 @@ public class MessageManager {
 		}
 	}
 
-	public void sendDelayedTeleportMessage(Player player) {
-		HashMap<String,String> vars = new HashMap<>();
-		vars.put("DELAY",plugin.getGroupManager().getGroup(player).getGuildTeleportDelay()+"");
-		Message.CHAT_DELAYEDTELEPORT.vars(vars).send(player);
-	}
-
-	public void sendNoPermissionsMessage(CommandSender sender) {
-		Message.CHAT_NOPERMISSIONS.send(sender);
-	}
-
-//	public void sendNoPermissionsMessage(Player player) {
-//		sendMessagesMsg(player, Message.CHAT_NOPERMISSIONS);
-//	}
-
-	public void sendUsageMessage(CommandSender sender, String path) {
+	public static void sendUsageMessage(CommandSender sender, String path) {
 		sender.sendMessage(StringUtils.fixColors(getMessagesString("chat.usage." + path)));
-	}
-
-	public String getLang() {
-		return lang;
 	}
 }
