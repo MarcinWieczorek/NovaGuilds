8b377863ae69a3c772707b8613e505e12a51368e
Marcin <marcin@marcin.co>
2015-08-17 17:11:17 +0200

    + YamlParseTest

---
+++ src/test/java/co/marcin/novaguilds/YamlParseTest.java
@@ -0,0 +1,51 @@
+package co.marcin.novaguilds;
+
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class YamlParseTest {
+	private static final File resourcesDirectory = new File("./src/main/resources/");
+
+	@Test
+	public void testConfig() throws FileNotFoundException, InvalidConfigurationException {
+		File configFile = new File(resourcesDirectory, "config.yml");
+
+		if(!configFile.exists()) {
+			throw new FileNotFoundException("Config file does not exist.");
+		}
+
+		try {
+			YamlConfiguration.loadConfiguration(configFile);
+		}
+		catch(NullPointerException e) {
+			throw new InvalidConfigurationException("Invalid YAML file ("+configFile.getPath()+")");
+		}
+	}
+
+	@Test
+	public void testLangs() throws NullPointerException, InvalidConfigurationException, FileNotFoundException {
+		File langsDir = new File(resourcesDirectory, "/lang");
+
+		if(langsDir.isDirectory()) {
+			File[] list = langsDir.listFiles();
+
+			if(list != null) {
+				for(File lang : list) {
+					try {
+						YamlConfiguration.loadConfiguration(lang);
+					}
+					catch(NullPointerException e) {
+						throw new InvalidConfigurationException("Invalid YAML file ("+lang.getPath()+")");
+					}
+				}
+			}
+		}
+		else {
+			throw new FileNotFoundException("Lang dir does not exist.");
+		}
+	}
+}
