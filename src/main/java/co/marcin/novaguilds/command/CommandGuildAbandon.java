52bf65ab807bb19410129c52ab16c71ed831eb7f
Marcin <marcin@marcin.co>
2015-08-17 05:29:17 +0200

    GuildAbandon cleanup

--- src/main/java/co/marcin/novaguilds/command/CommandGuildAbandon.java
+++ src/main/java/co/marcin/novaguilds/command/CommandGuildAbandon.java
@@ -4,6 +4,7 @@ import co.marcin.novaguilds.NovaGuilds;
 import co.marcin.novaguilds.basic.NovaGuild;
 import co.marcin.novaguilds.basic.NovaPlayer;
 import co.marcin.novaguilds.enums.AbandonCause;
+import co.marcin.novaguilds.enums.Commands;
 import co.marcin.novaguilds.enums.Message;
 import co.marcin.novaguilds.event.GuildAbandonEvent;
 import org.bukkit.command.Command;
@@ -15,46 +16,54 @@ import java.util.HashMap;
 
 public class CommandGuildAbandon implements CommandExecutor {
 	private static NovaGuilds plugin;
+	private static Commands command = Commands.GUILD_ABANDON;
 	
 	public CommandGuildAbandon(NovaGuilds novaGuilds) {
 		plugin = novaGuilds;
 	}
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if(!(sender instanceof Player)) {
+		if(!command.allowedSender(sender)) {
 			Message.CHAT_CMDFROMCONSOLE.send(sender);
 			return true;
 		}
-		
-		NovaPlayer nPlayer = plugin.getPlayerManager().getPlayer(sender);
-		if(nPlayer.hasGuild()) {
-			NovaGuild guild = nPlayer.getGuild();
-			
-			if(nPlayer.isLeader()) { //All passed
-				//fire event
-				GuildAbandonEvent guildAbandonEvent = new GuildAbandonEvent(guild, AbandonCause.PLAYER);
-				plugin.getServer().getPluginManager().callEvent(guildAbandonEvent);
 
-				//if event is not cancelled
-				if(!guildAbandonEvent.isCancelled()) {
-					plugin.getGuildManager().deleteGuild(guild);
-
-					Message.CHAT_GUILD_ABANDONED.send(sender);
-
-					HashMap<String, String> vars = new HashMap<>();
-					vars.put("PLAYER", sender.getName());
-					vars.put("GUILDNAME", guild.getName());
-					Message.BROADCAST_GUILD_ABANDONED.vars(vars).broadcast();
-					plugin.tagUtils.refreshAll();
-				}
-			}
-			else {
-				Message.CHAT_GUILD_NOTLEADER.send(sender);
-			}
+		if(!command.hasPermission(sender)) {
+			Message.CHAT_NOPERMISSIONS.send(sender);
+			return true;
 		}
-		else {
+
+		NovaPlayer nPlayer = NovaPlayer.get(sender);
+
+		if(!nPlayer.hasGuild()) {
 			Message.CHAT_GUILD_NOTINGUILD.send(sender);
+			return true;
 		}
+
+		NovaGuild guild = nPlayer.getGuild();
+			
+		if(!nPlayer.isLeader()) {
+			Message.CHAT_GUILD_NOTLEADER.send(sender);
+			return true;
+		}
+
+		//fire event
+		GuildAbandonEvent guildAbandonEvent = new GuildAbandonEvent(guild, AbandonCause.PLAYER);
+		plugin.getServer().getPluginManager().callEvent(guildAbandonEvent);
+
+		//if event is not cancelled
+		if(!guildAbandonEvent.isCancelled()) {
+			plugin.getGuildManager().deleteGuild(guild);
+
+			Message.CHAT_GUILD_ABANDONED.send(sender);
+
+			HashMap<String, String> vars = new HashMap<>();
+			vars.put("PLAYER", sender.getName());
+			vars.put("GUILDNAME", guild.getName());
+			Message.BROADCAST_GUILD_ABANDONED.vars(vars).broadcast();
+			plugin.tagUtils.refreshAll();
+		}
+
 		return true;
 	}
 	
