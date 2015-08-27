a4871f0f607b43a00d50595e8533b3b1f6cc068e
Marcin <marcin@marcin.co>
2015-08-27 17:56:32 +0200

    Better displaying top guilds in /gg

--- src/main/java/co/marcin/novaguilds/manager/CustomCommandManager.java
+++ src/main/java/co/marcin/novaguilds/manager/CustomCommandManager.java
@@ -95,25 +95,12 @@ public class CustomCommandManager {
 		return guiCommands.keySet();
 	}
 
-	public void updateGuiTop() {int limit = Integer.parseInt(Message.HOLOGRAPHICDISPLAYS_TOPGUILDS_TOPROWS.get()); //TODO move to config
+	public void updateGuiTop() {
 		guiCommands.remove(topItem);
-
-		int i=1;
-		List<String> lore = new ArrayList<>();
-		HashMap<String, String> vars = new HashMap<>();
-
-		for(NovaGuild guild : plugin.getGuildManager().getTopGuildsByPoints(limit)) {
-			vars.clear();
-			vars.put("GUILDNAME", guild.getName());
-			vars.put("N", String.valueOf(i));
-			vars.put("POINTS", String.valueOf(guild.getPoints()));
-			lore.add(Message.HOLOGRAPHICDISPLAYS_TOPGUILDS_ROW.vars(vars).get());
-			i++;
-		}
 
 		ItemMeta meta = Bukkit.getItemFactory().getItemMeta(topItem.getType());
 		meta.setDisplayName(Message.HOLOGRAPHICDISPLAYS_TOPGUILDS_HEADER.prefix(false).get());
-		meta.setLore(lore);
+		meta.setLore(plugin.getGuildManager().getTopGuilds());
 		topItem.setItemMeta(meta);
 		guiCommands.put(topItem, "g top");
 	}
