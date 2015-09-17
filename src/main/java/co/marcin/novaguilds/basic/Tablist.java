509495b03d8cc7a698624630826e8740d59abf58
marcin <marcin@marcin.co>
2015-09-17 18:10:07 +0200

    Tablist cleanup

--- src/main/java/co/marcin/novaguilds/basic/Tablist.java
+++ src/main/java/co/marcin/novaguilds/basic/Tablist.java
@@ -1,23 +1,19 @@
 package co.marcin.novaguilds.basic;
 
-import co.marcin.novaguilds.NovaGuilds;
-import co.marcin.novaguilds.basic.NovaPlayer;
 import co.marcin.novaguilds.enums.Config;
-import co.marcin.novaguilds.util.LoggerUtils;
 import co.marcin.novaguilds.util.StringUtils;
 import co.marcin.novaguilds.util.reflect.PacketSender;
 import co.marcin.novaguilds.util.reflect.packet.PacketPlayOutPlayerInfo;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class Tablist {
 	private final List<String> lines = new ArrayList<>();
 	private final NovaPlayer nPlayer;
-	private static int ping = 0;
 
 	public Tablist(NovaPlayer nPlayer) {
 		this.nPlayer = nPlayer;
@@ -41,7 +37,7 @@ public class Tablist {
 		lines.clear();
 
 //		lines.addAll(Config.TABLIST_SCHEME.getStringList());
-		Player[] op = Bukkit.getOnlinePlayers();
+		Player[] op = Bukkit.getOnlinePlayers().toArray(new Player[Bukkit.getOnlinePlayers().size()]);
 
 		HashMap<String, String> vars = new HashMap<>();
 		vars.put("ONLINE", String.valueOf(op.length));
@@ -64,6 +60,7 @@ public class Tablist {
 	private static Object[] packets(String[] ss, boolean b) {
 		Object[] packets = new Object[ss.length];
 		for(int i = 0; i < ss.length; i++) {
+			int ping = 0;
 			packets[i] = PacketPlayOutPlayerInfo.getPacket(ss[i], b, ping);
 		}
 
@@ -72,7 +69,7 @@ public class Tablist {
 
 	public static void patch() {
 		for(Player tPlayer : Bukkit.getOnlinePlayers()) {
-			NovaPlayer tnPlayer = NovaPlayer.get(tPlayer);
+//			NovaPlayer tnPlayer = NovaPlayer.get(tPlayer);
 			List<String> l = new ArrayList<>();
 
 			for(Player player : Bukkit.getOnlinePlayers()) {
