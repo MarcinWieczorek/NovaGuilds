9c2c7811ae557ef2d38fc1c0f8b5d38344b3f3dd
Marcin <marcin@marcin.co>
2015-09-30 22:10:58 +0200

    + Message when adding a top hologram

--- src/main/java/co/marcin/novaguilds/command/admin/hologram/CommandAdminHologramAddTop.java
+++ src/main/java/co/marcin/novaguilds/command/admin/hologram/CommandAdminHologramAddTop.java
@@ -1,10 +1,14 @@
 package co.marcin.novaguilds.command.admin.hologram;
 
+import co.marcin.novaguilds.basic.NovaHologram;
 import co.marcin.novaguilds.enums.Commands;
 import co.marcin.novaguilds.enums.Message;
 import co.marcin.novaguilds.interfaces.Executor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CommandAdminHologramAddTop implements Executor {
 	private final Commands command;
@@ -26,6 +30,11 @@ public class CommandAdminHologramAddTop implements Executor {
 			return;
 		}
 
-		plugin.getHologramManager().addTopHologram(((Player) sender).getLocation());
+		NovaHologram hologram = plugin.getHologramManager().addTopHologram(((Player) sender).getLocation());
+
+		Map<String, String> vars = new HashMap<>();
+		vars.put("NAME", hologram.getName());
+
+		Message.CHAT_ADMIN_HOLOGRAM_ADD_SUCCESS.vars(vars).send(sender);
 	}
 }
