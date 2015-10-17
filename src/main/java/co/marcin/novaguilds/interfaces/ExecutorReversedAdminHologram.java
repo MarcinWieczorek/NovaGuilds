30e97c0de65a507f9d48464913d9780a5a746129
Marcin <marcin@marcin.co>
2015-10-17 22:24:15 +0200

    + Executor for reversed hologram cmds

---
+++ src/main/java/co/marcin/novaguilds/interfaces/ExecutorReversedAdminHologram.java
@@ -0,0 +1,7 @@
+package co.marcin.novaguilds.interfaces;
+
+import co.marcin.novaguilds.basic.NovaHologram;
+
+public interface ExecutorReversedAdminHologram {
+	void hologram(NovaHologram hologram);
+}
