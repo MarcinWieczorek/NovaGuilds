f70bb0b199a95198a4a812d6f8262d84c458160b
Marcin Wieczorek <marcin@marcin.co>
2015-12-31 02:37:24 +0100

    Cancel tool progress on world change

--- src/main/java/co/marcin/novaguilds/listener/MoveListener.java
+++ src/main/java/co/marcin/novaguilds/listener/MoveListener.java
@@ -25,6 +25,7 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
 
@@ -44,6 +45,11 @@ public class MoveListener implements Listener {
 	@EventHandler
 	public void onPlayerTeleport(PlayerTeleportEvent event) {
 		execute(event);
+	}
+
+	@EventHandler
+	public void onPlayerChangedWorld(PlayerChangedWorldEvent event) {
+		NovaPlayer.get(event.getPlayer()).cancelToolProgress();
 	}
 
 	private void execute(PlayerMoveEvent event) {
