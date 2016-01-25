1f4e932b4efaad206674d578f9d2db6dfb2c0de1
Marcin Wieczorek <marcin@marcin.co>
2016-01-25 01:31:52 +0100

    TimeStringConversionTest +Random tests

--- src/test/java/co/marcin/novaguilds/TimeStringConversionTest.java
+++ src/test/java/co/marcin/novaguilds/TimeStringConversionTest.java
@@ -19,6 +19,7 @@
 package co.marcin.novaguilds;
 
 import co.marcin.novaguilds.manager.MessageManager;
+import co.marcin.novaguilds.util.NumberUtils;
 import co.marcin.novaguilds.util.StringUtils;
 import co.marcin.novaguilds.yaml.YamlParseTest;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -60,10 +61,18 @@ public class TimeStringConversionTest {
 			"1 year 1 month 1 week 1 day 1 hour 1 minute 1 second",
 	};
 
+	static {
+		MessageManager messageManager = new MessageManager();
+		File langsDir = new File(YamlParseTest.resourcesDirectory, "/lang");
+		File motherLangFile = new File(langsDir, "en-en.yml");
+		YamlConfiguration motherConfiguration = YamlConfiguration.loadConfiguration(motherLangFile);
+		messageManager.setMessages(motherConfiguration);
+	}
+
 	@Test
 	public void testStringToSeconds() throws Exception {
 		System.out.println("Testing stringToSeconds()");
-		for(int i=0; i<strings.length; i++) {
+		for(int i = 0; i < strings.length; i++) {
 			System.out.println(strings[i] + "->" + StringUtils.stringToSeconds(strings[i]) + " = " + longs[i]);
 			if(StringUtils.stringToSeconds(strings[i]) != longs[i]) {
 				throw new Exception("Values not equal!");
@@ -77,17 +86,49 @@ public class TimeStringConversionTest {
 	public void testSecondsToString() throws Exception {
 		System.out.println("Testing secondsToString()");
 
-		MessageManager messageManager = new MessageManager();
-		File langsDir = new File(YamlParseTest.resourcesDirectory, "/lang");
-		File motherLangFile = new File(langsDir, "en-en.yml");
-		YamlConfiguration motherConfiguration = YamlConfiguration.loadConfiguration(motherLangFile);
-		messageManager.setMessages(motherConfiguration);
-
-		for(int i=0; i<strings2.length; i++) {
+		for(int i = 0; i < strings2.length; i++) {
 			System.out.println(longs[i] + "->" + StringUtils.secondsToString(longs[i]) + " = " + strings2[i]);
 			if(!StringUtils.secondsToString(longs[i]).equals(strings2[i])) {
 				throw new Exception("Values not equal!");
 			}
+		}
+
+		System.out.println();
+	}
+
+	@Test
+	public void testRandom() throws Exception {
+		for(int i = 1; i < 100; i++) {
+			int seconds = NumberUtils.randInt(0, 100000000);
+
+			String str = StringUtils.secondsToString(seconds);
+
+			String str2;
+			str2 = org.apache.commons.lang.StringUtils.replace(str, " seconds", "s");
+			str2 = org.apache.commons.lang.StringUtils.replace(str2, " minutes", "m");
+			str2 = org.apache.commons.lang.StringUtils.replace(str2, " hours", "h");
+			str2 = org.apache.commons.lang.StringUtils.replace(str2, " days", "d");
+			str2 = org.apache.commons.lang.StringUtils.replace(str2, " weeks", "w");
+			str2 = org.apache.commons.lang.StringUtils.replace(str2, " months", "mo");
+			str2 = org.apache.commons.lang.StringUtils.replace(str2, " years", "y");
+
+			str2 = org.apache.commons.lang.StringUtils.replace(str2, " second", "s");
+			str2 = org.apache.commons.lang.StringUtils.replace(str2, " minute", "m");
+			str2 = org.apache.commons.lang.StringUtils.replace(str2, " hour", "h");
+			str2 = org.apache.commons.lang.StringUtils.replace(str2, " day", "d");
+			str2 = org.apache.commons.lang.StringUtils.replace(str2, " week", "w");
+			str2 = org.apache.commons.lang.StringUtils.replace(str2, " month", "mo");
+			str2 = org.apache.commons.lang.StringUtils.replace(str2, " year", "y");
+
+			if(StringUtils.stringToSeconds(str2) != seconds) {
+				System.out.println("Failed case:");
+				System.out.println(" Seconds: " + seconds);
+				System.out.println(" String #1: " + str);
+				System.out.println(" String #2: " + str2);
+				throw new Exception("Failed Random!");
+			}
+
+			System.out.println(seconds + " " + str);
 		}
 
 		System.out.println();
