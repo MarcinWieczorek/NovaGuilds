c2b3829ae041455dcb07e949d0f3004fa7a7c464
Marcin Wieczorek <marcin@marcin.co>
2016-06-18 22:55:52 +0200

    Improved SignGUI for 1.9-R1

--- src/main/java/co/marcin/novaguilds/impl/versionimpl/v1_9_R1/SignGUIImpl.java
+++ src/main/java/co/marcin/novaguilds/impl/versionimpl/v1_9_R1/SignGUIImpl.java
@@ -18,55 +18,27 @@
 
 package co.marcin.novaguilds.impl.versionimpl.v1_9_R1;
 
+import co.marcin.novaguilds.api.util.Packet;
 import co.marcin.novaguilds.event.PacketReceiveEvent;
 import co.marcin.novaguilds.impl.util.AbstractPacketHandler;
 import co.marcin.novaguilds.impl.util.signgui.AbstractSignGui;
+import co.marcin.novaguilds.impl.versionimpl.v1_9_R1.packet.PacketPlayInUpdateSign;
+import co.marcin.novaguilds.impl.versionimpl.v1_9_R1.packet.PacketPlayOutBlockChange;
+import co.marcin.novaguilds.impl.versionimpl.v1_9_R1.packet.PacketPlayOutOpenSignEditor;
+import co.marcin.novaguilds.impl.versionimpl.v1_9_R1.packet.PacketPlayOutUpdateSign;
 import co.marcin.novaguilds.util.LoggerUtils;
 import co.marcin.novaguilds.util.reflect.PacketSender;
-import co.marcin.novaguilds.util.reflect.Reflections;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
-import java.lang.reflect.Array;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
 @SuppressWarnings("ConstantConditions")
 public class SignGUIImpl extends AbstractSignGui {
-
-	protected static Class<?> packetInUpdateSignClass;
-	protected static Class<?> packetOutUpdateSignClass;
-	protected static Class<?> packetBlockChangeClass;
-	protected static Class<?> packetOpenSignEditorClass;
-	protected static Class<?> baseBlockPositionClass;
-	protected static Class<?> blockPositionClass;
-	protected static Class<?> worldClass;
-	protected static Class<?> chatComponentTextClass;
-	protected static Class<?> craftMagicNumbersClass;
-
-	static {
-		try {
-			packetInUpdateSignClass = Reflections.getCraftClass("PacketPlayInUpdateSign");
-			packetOutUpdateSignClass = Reflections.getCraftClass("PacketPlayOutUpdateSign");
-			packetBlockChangeClass = Reflections.getCraftClass("PacketPlayOutBlockChange");
-			packetOpenSignEditorClass = Reflections.getCraftClass("PacketPlayOutOpenSignEditor");
-			baseBlockPositionClass = Reflections.getCraftClass("BaseBlockPosition");
-			blockPositionClass = Reflections.getCraftClass("BlockPosition");
-			worldClass = Reflections.getCraftClass("World");
-			chatComponentTextClass = Reflections.getCraftClass("ChatComponentText");
-			craftMagicNumbersClass = Reflections.getBukkitClass("util.CraftMagicNumbers");
-		}
-		catch(Exception e) {
-			LoggerUtils.exception(e);
-		}
-	}
-
 	public SignGUIImpl() {
 		registerUpdateHandling();
 	}
@@ -74,29 +46,26 @@ public class SignGUIImpl extends AbstractSignGui {
 	@Override
 	public void open(Player player, String[] defaultText, SignGUIListener response) {
 		try {
-			List<Object> packets = new ArrayList<>();
+			List<Packet> packets = new ArrayList<>();
 			Location location = player.getLocation().clone();
 			location.setY(0);
 
-			Object blockPosition = blockPosition(location);
-
 			if(defaultText != null) {
-				packets.add(packetBlockChange(location, Material.SIGN_POST, 0));
-				packets.add(packetSignChange(location, defaultText));
+				packets.add(new PacketPlayOutBlockChange(location, Material.SIGN_POST, 0));
+				packets.add(new PacketPlayOutUpdateSign(location, defaultText));
 			}
 
-			Object packetOpenSignEditor = packetOpenSignEditorClass.getConstructor(blockPositionClass).newInstance(blockPosition);
-			packets.add(packetOpenSignEditor);
+			packets.add(new PacketPlayOutOpenSignEditor(location));
 
 			if(defaultText != null) {
-				packets.add(packetBlockChange(location, null, 0));
+				packets.add(new PacketPlayOutBlockChange(location, null, 0));
 			}
 
 			signLocations.put(player.getUniqueId(), location);
 			listeners.put(player.getUniqueId(), response);
-			PacketSender.sendPacket(player, packets.toArray());
+			PacketSender.sendPacket(player, packets.toArray(new Packet[packets.size()]));
 		}
-		catch(NoSuchMethodException | IllegalAccessException | InstantiationException | InvocationTargetException e) {
+		catch(NoSuchFieldException | NoSuchMethodException | IllegalAccessException | InstantiationException | InvocationTargetException e) {
 			LoggerUtils.exception(e);
 		}
 	}
@@ -106,42 +75,24 @@ public class SignGUIImpl extends AbstractSignGui {
 			@Override
 			public void handle(PacketReceiveEvent event) {
 				try {
-					if(event.getPacketName().equals("PacketPlayInUpdateSign")) {
-						Object packet = event.getPacket();
+					final PacketPlayInUpdateSign packetPlayInUpdateSign = new PacketPlayInUpdateSign(event.getPacket());
+					final Player player = event.getPlayer();
 
-						Field blockPositionField = Reflections.getPrivateField(packetInUpdateSignClass, "a");
-						Reflections.FieldAccessor<String[]> linesField = Reflections.getField(packetInUpdateSignClass, String[].class, 0);
-						Field xField = Reflections.getPrivateField(baseBlockPositionClass, "a");
-						Field yField = Reflections.getPrivateField(baseBlockPositionClass, "c");
-						Field zField = Reflections.getPrivateField(baseBlockPositionClass, "d");
+					Location v = getSignLocations().remove(player.getUniqueId());
 
-						final Player player = event.getPlayer();
-						Location v = getSignLocations().remove(player.getUniqueId());
+					if(v == null || packetPlayInUpdateSign.getX() != v.getBlockX() || packetPlayInUpdateSign.getY() != v.getBlockY() || packetPlayInUpdateSign.getZ() != v.getBlockZ()) {
+						return;
+					}
 
-						if(v == null) {
-							return;
-						}
+					final SignGUIListener response = getListeners().remove(player.getUniqueId());
 
-						Object blockPosition = blockPositionField.get(packet);
-						int x = xField.getInt(blockPosition);
-						int y = yField.getInt(blockPosition);
-						int z = zField.getInt(blockPosition);
-
-						if(x != v.getBlockX() || y != v.getBlockY() || z != v.getBlockZ()) {
-							return;
-						}
-
-						final String[] lines = linesField.get(packet);
-						final SignGUIListener response = getListeners().remove(event.getPlayer().getUniqueId());
-
-						if(response != null) {
-							event.setCancelled(true);
-							Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
-								public void run() {
-									response.onSignDone(player, lines);
-								}
-							});
-						}
+					if(response != null) {
+						event.setCancelled(true);
+						Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+							public void run() {
+								response.onSignDone(player, packetPlayInUpdateSign.getLines());
+							}
+						});
 					}
 				}
 				catch(IllegalAccessException | NoSuchFieldException e) {
@@ -149,103 +100,5 @@ public class SignGUIImpl extends AbstractSignGui {
 				}
 			}
 		};
-	}
-
-	protected Object getData(Material material, int data) {
-		try {
-			Method getBlockMethod = Reflections.getMethod(craftMagicNumbersClass, "getBlock", Material.class);
-			Object block = getBlockMethod.invoke(craftMagicNumbersClass, material);
-			Method fromLegacyDataMethod = Reflections.getMethod(block.getClass(), "fromLegacyData");
-			return fromLegacyDataMethod.invoke(block, data);
-		}
-		catch(IllegalAccessException | InvocationTargetException e) {
-			LoggerUtils.exception(e);
-			return null;
-		}
-	}
-
-	protected Object packetBlockChange(Location location, Material material, int data) {
-		try {
-			Object blockPosition = blockPosition(location);
-			Object packet = packetBlockChangeClass.newInstance();
-			Field blockPositionField = Reflections.getField(packetBlockChangeClass, "a");
-			Field blockDataField = Reflections.getField(packetBlockChangeClass, "block");
-			blockPositionField.setAccessible(true);
-			blockDataField.setAccessible(true);
-
-			Object blockData;
-			if(material == null) {
-				Object world = Reflections.getHandle(location.getWorld());
-				Method getTypeMethod = Reflections.getMethod(worldClass, "getType");
-				blockData = getTypeMethod.invoke(world, blockPosition);
-			}
-			else {
-				blockData = getData(material, data);
-			}
-
-			blockPositionField.set(packet, blockPosition);
-			blockDataField.set(packet, blockData);
-
-			return packet;
-		}
-		catch(InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchFieldException e) {
-			LoggerUtils.exception(e);
-			return null;
-		}
-	}
-
-	protected Object packetSignChange(Location location, String[] lines) {
-		try {
-			Object blockPosition = blockPosition(location);
-			Object packet = packetOutUpdateSignClass.newInstance();
-
-			Field worldField = Reflections.getField(packetOutUpdateSignClass, "a");
-			Field blockPositionField = Reflections.getField(packetOutUpdateSignClass, "b");
-			Field linesField = Reflections.getField(packetOutUpdateSignClass, "c");
-
-			worldField.setAccessible(true);
-			blockPositionField.setAccessible(true);
-			linesField.setAccessible(true);
-
-
-			Constructor<?> chatComponentTextConstructor = chatComponentTextClass.getConstructor(String.class);
-
-			worldField.set(packet, Reflections.getHandle(location.getWorld()));
-			blockPositionField.set(packet, blockPosition);
-
-			int n = 4;
-			Object array = Array.newInstance(chatComponentTextClass, n);
-			for(int i = 0; i < n; i++) {
-				Object val = chatComponentTextConstructor.newInstance(lines[i]);
-
-				Array.set(array, i, val);
-			}
-
-			linesField.set(packet, array);
-
-			return packet;
-		}
-		catch(InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException | NoSuchFieldException e) {
-			LoggerUtils.exception(e);
-			return null;
-		}
-	}
-
-	protected Object blockPosition(Location location) {
-		try {
-			return blockPositionClass.getConstructor(
-					int.class,
-					int.class,
-					int.class
-			).newInstance(
-					location.getBlockX(),
-					location.getBlockY(),
-					location.getBlockZ()
-			);
-		}
-		catch(InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
-			LoggerUtils.exception(e);
-			return null;
-		}
 	}
 }
