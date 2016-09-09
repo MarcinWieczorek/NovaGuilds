57592c293fa74e0a9b5fc4ce5bc60609b328ecc3
Marcin Wieczorek <marcin@marcin.co>
2016-09-10 01:13:29 +0200

    Fix resetting bar style and color

--- src/main/java/co/marcin/novaguilds/manager/ConfigManager.java
+++ src/main/java/co/marcin/novaguilds/manager/ConfigManager.java
@@ -255,13 +255,13 @@ public class ConfigManager {
 
 		//Bar style enum
 		if(getServerVersion().isNewerThan(ServerVersion.MINECRAFT_1_8_R2)) {
-			if(Config.BOSSBAR_RAIDBAR_STYLE.toEnum(BarColor.class) == null) {
+			if(Config.BOSSBAR_RAIDBAR_STYLE.toEnum(BarStyle.class) == null) {
 				LoggerUtils.error("Invalid BarStyle enum. Resetting to default.");
 				Config.BOSSBAR_RAIDBAR_STYLE.set(BarStyle.SOLID.name());
 			}
 
 			//Bar color enum
-			if(Config.BOSSBAR_RAIDBAR_COLOR.toEnum(BarStyle.class) == null) {
+			if(Config.BOSSBAR_RAIDBAR_COLOR.toEnum(BarColor.class) == null) {
 				LoggerUtils.error("Invalid BarColor enum. Resetting to default.");
 				Config.BOSSBAR_RAIDBAR_COLOR.set(BarColor.PURPLE.name());
 			}
