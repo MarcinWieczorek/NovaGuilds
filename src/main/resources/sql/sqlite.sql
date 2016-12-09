9182319668eddb410247ae9ff54ba540a0de7ae2
Marcin Wieczorek <marcin@marcin.co>
2016-12-09 21:35:33 +0100

    Change banner field to text

--- src/main/resources/sql/sqlite.sql
+++ src/main/resources/sql/sqlite.sql
@@ -21,7 +21,7 @@ CREATE TABLE `{SQLPREFIX}guilds` (
   `bankloc` tinytext NOT NULL,
   `slots` tinytext NOT NULL,
   `openinv` int(1) NOT NULL
-  `banner` tinytext NOT NULL,
+  `banner` text NOT NULL,
 );
 --
 CREATE TABLE `{SQLPREFIX}players` (
